e5fc84c735be5404c5eea0f6e9c84c7e151953c2
ntwali <ntwalihatsor78.nh@gmail.com>
2025-09-04 15:33:56 +0200

    dropped from ~2 sec to ~500ms

--- main.go
+++ main.go
@@ -3,15 +3,40 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"sync"
+	"time"
 
 	api "github.com/ntwaliheritier/giphy_scrapper/api"
 )
 
 func main() {
-	data := api.FetchGif("bear", 3)
-	b, err := json.MarshalIndent(data, "", "  ") 
-	if err != nil {
-		panic(err)
+	gifs := []string{"bear", "mouse", "people", "computer", "football"}
+	var wg sync.WaitGroup
+	dataChannel := make(chan api.Response)
+
+	startTime := time.Now()
+	go func() {
+		for _, gif := range gifs {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				data := api.FetchGif(gif, 3)
+
+				dataChannel <- data
+			}()
+		}
+		wg.Wait()
+		close(dataChannel)
+	}()
+
+	for data := range dataChannel {
+		b, err := json.MarshalIndent(data, "", " ")
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(b))
+		fmt.Println("============")
 	}
-	fmt.Println(string(b))
+
+	fmt.Printf("It took %v to fetch results", time.Since(startTime))
 }
